4ccf3a65e49dfee5931234e57b075f9d23e67d76
Thanh An~ <sunthanhan1308@gmail.com>
2022-06-01 09:21:59 +0700

    title change

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,18 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-import Header from './../components/Header';
-import Head from 'next/head';
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import Header from "./../components/Header";
+import Head from "next/head";
 function MyApp({ Component, pageProps }: AppProps) {
-  return (
-      <>
-          <Head>
-              <title>Medium</title>
-              <link rel="icon" href="/favicon.ico" />
-          </Head>
-          <Header />
-          <Component {...pageProps} />
-      </>
-  );
+    return (
+        <>
+            <Head>
+                <title>Journey to the end of life</title>
+                <link rel="icon" href="/coin.ico" />
+            </Head>
+            <Header />
+            <Component {...pageProps} />
+        </>
+    );
 }
 
-export default MyApp
+export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -16,10 +16,9 @@ export default function Home({ posts }: Props) {
             <div className="flex justify-between items-center bg-yellow-400 border-y border-black py-10 lg:py-0">
                 <div className="px-10 space-y-5">
                     <h1 className="text-6xl max-w-xl font-serif">
-                        <span className="underline decoration-black decoration-4">Medium</span> is a place to write,
-                        read and connect
+                        <span className="underline decoration-black decoration-4">Mefo Blog</span> is a place where I write things I learned and experienced.
                     </h1>
-                    <h2>It's easy and free to post your thinking on any topic and connect with millions of readers</h2>
+                    <h2>I'm a developer who love to play games and explore new things'</h2>
                 </div>
                 <img
                     className="hidden md:inline-flex h-32 lg:h-full"
