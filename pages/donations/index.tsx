df0a4f3c7eb444b3a5edaefce5632865efce6f5d
Thanh An~ <sunthanhan1308@gmail.com>
2022-06-01 16:35:56 +0700

    donation responsive

--- pages/donations/index.tsx
+++ pages/donations/index.tsx
@@ -1,13 +1,12 @@
 
 function index() {
     return (
-        <div className="max-w-7xl mx-auto relative">
+        <div className="md:max-w-7xl max-w-xl mx-auto relative">
             <img src="https://i.ibb.co/6mztxR5/donations.jpg" alt=""></img>
-            <div style={{ fontFamily: "Valorant", bottom: "10%" }} className="absolute inset-x-1/3 lg:bottom-50">
-                <h1 className=" text-sm  lg:text-3xl ">PLEASE DONATE FOR <br/> NEW VALORANT SKINS</h1>
-                <br />
-                <h1 className=" text-sm  lg:text-2xl animate-bounce transition ease-in-out">MOMO: 0978699454</h1>
-                <h1 className=" text-sm  lg:text-2xl animate-bounce transition ease-in-out">BANK: 6211000860963</h1>
+            <div style={{ fontFamily: "Valorant", bottom: "10%" }} className="absolute inset-x-1/3 lg:bottom-50 ">          
+                <p className="hidden md:inline text-xl lg:text-2xl ">PLEASE DONATE FOR <br/> NEW VALORANT SKINS</p>
+                <p className="text-xs sm:text-md md:text-xl animate-bounce transition ease-in-out"><span className="text-pink-600">MOMO</span>: 0978699454</p>
+                <p className="text-xs sm:text-md md:text-xl animate-bounce transition ease-in-out"><span className="text-blue-600">BANK</span>: 6211000860963</p>
             </div>
         </div>
     );
