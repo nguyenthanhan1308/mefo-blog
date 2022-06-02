ee8b58775819f9498875b05e4b3a1fbff2adef64
Thanh An~ <sunthanhan1308@gmail.com>
2022-06-02 13:50:00 +0700

    momo qr

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
     images: {
-        domains: ["https://links.papareact.com", "https://i.ibb.co"],
+        domains: ["https://links.papareact.com", "i.ibb.co"],
     },
     reactStrictMode: true,
 };
--- pages/donations/index.tsx
+++ pages/donations/index.tsx
@@ -1,9 +1,9 @@
-
+import Image from'next/image'; 
 function index() {
     return (
-        <div className="md:max-w-7xl max-w-xl mx-auto relative">
+        <div className="md:max-w-7xl max-w-xl mx-auto relative my-10">
             <img
-                className="border-x border-black"
+                className="border border-black"
                 src="https://i.ibb.co/6mztxR5/donations.jpg"
                 alt="credit"
                 useMap="#image-map"
@@ -18,7 +18,10 @@ function index() {
                     shape="rect"
                 />
             </map>
-            <div style={{ fontFamily: "Valorant", bottom: "10%" }} className="absolute inset-x-1/3 lg:bottom-50 ">
+            <div
+                style={{ fontFamily: "Valorant" }}
+                className="absolute bottom-[0] inset-x-1/3 lg:bottom-[2%] child:mx-auto"
+            >
                 <p className="hidden md:inline text-xl lg:text-2xl ">
                     PLEASE DONATE FOR <br /> NEW VALORANT SKINS
                 </p>
@@ -28,6 +31,11 @@ function index() {
                 <p className="text-xs sm:text-md md:text-xl animate-bounce transition ease-in-out">
                     <span className="text-blue-600">BIDV</span>: 6211000860963
                 </p>
+                <img
+                    className="h-[80px] w-[80px] xxs:h-[100px] xxs:w-[100px] xl:h-[160px] xl:w-[160px] "
+                    src="https://i.ibb.co/qJcvJF6/momo-jpg.png"
+                    alt="momo-jpg"
+                ></img>
             </div>
         </div>
     );
