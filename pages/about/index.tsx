66b27afbf6a467989edf03ec7758287bee43e751
Thanh An~ <sunthanhan1308@gmail.com>
2022-06-03 15:01:01 +0700

    about

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -9,10 +9,10 @@ function index() {
                 <hr className="max-w-lg w-full my-5 mx-auto border border-green-500" />
             </div>
             <div className="width-trans group transition duration-200 flex justify-center md:h-[242px] lg:h-[330px] xl:h-[420px]">
-                <div className=" box-border trans-item w-[364px]  group-hover:w-[485px] lg:w-[492px] lg:group-hover:w-[656px] xl:w-[640px] xl:group-hover:w-[855px]">
-                    <img src="https://i.ibb.co/fGPSnKQ/chill.jpg" alt="chill" />
+                <div className=" shadow-lg shadow-black box-border trans-item w-[364px]  group-hover:w-[485px] lg:w-[492px] lg:group-hover:w-[656px] xl:w-[640px] xl:group-hover:w-[855px]">
+                    <img className="" src="https://i.ibb.co/fGPSnKQ/chill.jpg" alt="chill" />
                 </div>
-                <div className="relative trans-item pl-4  text-xl w-[364px] group-hover:w-[240px] lg:w-[492px] lg:group-hover:w-[328px] xl:w-[640px] xl:group-hover:w-[415px] ">
+                <div className="relative trans-item pl-4 text-2xl w-[364px] group-hover:w-[240px] lg:w-[492px] lg:group-hover:w-[328px] xl:w-[640px] xl:group-hover:w-[415px] ">
                     <p className="group-hover:opacity-0 transition duration-2 ease-in-out">A cat.</p>
                     <p className="group-hover:opacity-100 opacity-0 top-0 absolute transition duration-2 ease-in-out">
                         A Google researcher <br /> A Developer <br />
@@ -21,7 +21,7 @@ function index() {
             </div>
             <div className="second-trans">
                 <div className="width-trans group transition duration-200 flex justify-center md:h-[204px] lg:h-[276px] xl:h-[360px]">
-                    <div className="relative trans-item pr-4 text-xl text-right w-[364px] group-hover:w-[240px] lg:w-[492px] lg:group-hover:w-[328px] xl:w-[640px] xl:group-hover:w-[415px] ">
+                    <div className="relative trans-item pr-4 text-2xl text-right w-[364px] group-hover:w-[240px] lg:w-[492px] lg:group-hover:w-[328px] xl:w-[640px] xl:group-hover:w-[415px] ">
                         <p className="group-hover:opacity-0 transition duration-2 ease-in-out">A games enjoyer.</p>
                         <p className="group-hover:opacity-100 opacity-0 top-0 right-2 absolute transition duration-2 ease-in-out">
                             Valorant <br /> Csgo <br />
@@ -30,18 +30,22 @@ function index() {
                             Final Fantasy
                         </p>
                     </div>
-                    <div className="box-border trans-item w-[364px] group-hover:w-[485px] lg:w-[492px] lg:group-hover:w-[656px] xl:w-[640px] xl:group-hover:w-[855px]">
-                        <img className="aspect-video" src="https://i.ibb.co/vYBH3wH/cs-val.webp" alt="chill" />
+                    <div className=" box-border trans-item w-[364px] group-hover:w-[485px] lg:w-[492px] lg:group-hover:w-[656px] xl:w-[640px] xl:group-hover:w-[855px]">
+                        <img
+                            className="aspect-video shadow-lg shadow-black"
+                            src="https://i.ibb.co/vYBH3wH/cs-val.webp"
+                            alt="chill"
+                        />
                     </div>
                 </div>
-                <div className="second-trans  mx-auto flex justify-between w-full">
-                    <div className="habit text-lg shadow shadow-green-600 w-[240px]  lg:w-[330px] xl:w-[410px] ">
+                <div className="second-trans mx-auto flex justify-between w-full mb-5">
+                    <div className="habit text-lg shadow shadow-green-600 w-[240px] lg:w-[330px] xl:w-[410px] ">
                         Gaming
                     </div>
-                    <div className="habit text-lg shadow shadow-yellow-600 w-[240px]  lg:w-[330px]  xl:w-[410px] ">
+                    <div className="habit text-lg shadow shadow-yellow-600 w-[240px] lg:w-[330px] xl:w-[410px] ">
                         Programming
                     </div>
-                    <div className="habit text-lg shadow shadow-red-600 w-[240px]  lg:w-[330px] xl:w-[410px]">
+                    <div className="habit text-lg shadow shadow-red-600 w-[240px] lg:w-[330px] xl:w-[410px]">
                         Eating
                     </div>
                 </div>
