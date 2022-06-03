ff6ee0bc55c32a69cf45bb15518c04e23242e1f8
Thanh An~ <sunthanhan1308@gmail.com>
2022-06-03 15:29:51 +0700

    fix responsive

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -12,7 +12,7 @@ function index() {
                 <div className="box-border trans-item w-[364px]  group-hover:w-[485px] lg:w-[492px] lg:group-hover:w-[656px] xl:w-[640px] xl:group-hover:w-[855px]">
                     <img className="shadow-lg shadow-black" src="https://i.ibb.co/fGPSnKQ/chill.jpg" alt="chill" />
                 </div>
-                <div className="relative trans-item pl-4 text-2xl w-[364px] group-hover:w-[240px] lg:w-[492px] lg:group-hover:w-[328px] xl:w-[640px] xl:group-hover:w-[415px] ">
+                <div className="relative trans-item pl-4 text-sm md:text-2xl w-[364px] group-hover:w-[240px] lg:w-[492px] lg:group-hover:w-[328px] xl:w-[640px] xl:group-hover:w-[415px] ">
                     <p className="group-hover:opacity-0 transition duration-2 ease-in-out">A cat.</p>
                     <p className="group-hover:opacity-100 opacity-0 top-0 absolute transition duration-2 ease-in-out">
                         A Developer <br /> A Google researcher <br />
@@ -21,7 +21,7 @@ function index() {
             </div>
             <div className="second-trans">
                 <div className="width-trans group transition duration-200 flex justify-center md:h-[204px] lg:h-[276px] xl:h-[360px]">
-                    <div className="relative trans-item pr-4 text-2xl text-right w-[364px] group-hover:w-[240px] lg:w-[492px] lg:group-hover:w-[328px] xl:w-[640px] xl:group-hover:w-[415px] ">
+                    <div className="relative trans-item pr-4 text-sm md:text-2xl text-right w-[364px] group-hover:w-[240px] lg:w-[492px] lg:group-hover:w-[328px] xl:w-[640px] xl:group-hover:w-[415px] ">
                         <p className="group-hover:opacity-0 transition duration-2 ease-in-out">A games enjoyer.</p>
                         <p className="group-hover:opacity-100 opacity-0 top-0 right-2 absolute transition duration-2 ease-in-out">
                             lol <br />
@@ -43,13 +43,13 @@ function index() {
                 <div className="second-trans mx-auto  w-full mb-40">
                     <hr className="max-w-lg w-full my-5 mx-auto border border-green-500" />
                     <div className="flex justify-between">
-                        <div className="habit text-lg shadow shadow-green-600 w-[240px] lg:w-[330px] xl:w-[410px] ">
+                        <div className="habit text-xs md:text-lg shadow shadow-green-600 w-[130px] md:w-[245px] lg:w-[330px] xl:w-[410px] ">
                             Gaming
                         </div>
-                        <div className="habit text-lg shadow shadow-yellow-600 w-[240px] lg:w-[330px] xl:w-[410px] ">
+                        <div className="habit text-xs md:text-lg shadow shadow-yellow-600 w-[130px] md:w-[245px] lg:w-[330px] xl:w-[410px] ">
                             Programming
                         </div>
-                        <div className="habit text-lg shadow shadow-red-600 w-[240px] lg:w-[330px] xl:w-[410px]">
+                        <div className="habit text-xs md:text-lg shadow shadow-red-600 w-[130px] md:w-[245px] lg:w-[330px] xl:w-[410px]">
                             Eating
                         </div>
                     </div>
