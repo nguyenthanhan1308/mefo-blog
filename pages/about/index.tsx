9cc094a1768432349c955b41f3d2cc05a423dd12
Thanh An~ <sunthanhan1308@gmail.com>
2022-06-03 12:03:15 +0700

    about page

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -3,7 +3,7 @@ import Container from './../../components/Container';
 
 function index() {
     return (
-        <div className="max-w-7xl mx-auto  transition duration-300">
+        <div className="max-w-7xl mx-auto transition duration-300 p-2">
             <div className="text-sega flex flex-col my-5 ">
                 <p className="mx-auto text-4xl">ABOUT ME</p>
                 <hr className="max-w-lg w-full my-5 mx-auto border border-green-500" />
@@ -12,14 +12,14 @@ function index() {
                 <div className="trans-item w-[364px] group-hover:w-[485px] lg:w-[492px] lg:group-hover:w-[656px] xl:w-[640px] xl:group-hover:w-[855px]">
                     <img src="https://i.ibb.co/fGPSnKQ/chill.jpg" alt="chill" />
                 </div>
-                <div className="trans-item ml-5 text-xl w-[364px] group-hover:w-[240px] lg:w-[492px] lg:group-hover:w-[328px] xl:w-[640px] xl:group-hover:w-[415px]">
-                    A cat who love to wandering around.
+                <div className="trans-item ml-5 text-xl w-[364px] group-hover:w-[240px] lg:w-[492px] lg:group-hover:w-[328px] xl:w-[640px] xl:group-hover:w-[415px] ">
+                    A cat who love wandering around.
                 </div>
             </div>
-            <div className="second-trans mt-[5px] flex h-[400px]">
-                <div className="first habit shadow shadow-green-600  ">Gaming</div>
-                <div className="second habit shadow shadow-yellow-600  ">Programming</div>
-                <div className="third habit shadow shadow-red-600  ">Eating</div>
+            <div className="second-trans mt-[5px] mx-auto flex h-[400px] justify-between w-full">
+                <div className="habit shadow shadow-green-600 w-[240px] xl:w-[410px] ">Gaming</div>
+                <div className="habit shadow shadow-yellow-600 w-[240px] xl:w-[410px] ">Programming</div>
+                <div className="habit shadow shadow-red-600 w-[240px] xl:w-[410px]">Eating</div>
             </div>
         </div>
     );
