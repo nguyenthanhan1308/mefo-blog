67bbced54933ac8e28b6557765fed0d950b97cb2
Thanh An~ <sunthanhan1308@gmail.com>
2022-06-06 14:29:37 +0700

    contact

--- pages/contact/index.tsx
+++ pages/contact/index.tsx
@@ -1,9 +1,78 @@
-import React from 'react'
-
+import Link from "next/link";
 function index() {
     return (
-    <div>index</div>
-    )
+        <div className="md:max-w-7xl max-w-xl mx-auto relative my-10">
+            <div className="flex flex-col my-5 ">
+                <p className="mx-auto text-2xl md:text-4xl">CONTACT</p>
+                <hr className="max-w-lg w-full my-5 mx-auto border border-green-500" />
+            </div>
+            <div className="flex flex-col">
+                {/* discord */}
+                <div
+                    key="discord"
+                    className={`mb-5 rounded-full border h-24 w-24 mx-auto flex items-center bg-[#5865F2] text-white group hover:w-full hover:justify-start transition duration-300 ease-in-out trans-item cursor-pointer`}
+                >
+                    <img
+                        src={`https://i.ibb.co/sJySCwz/discord-logo-removebg-preview.png`}
+                        className={`ml-[15px] mt-2 w-16 h-16 object-cover  transition group-hover:animate-shaking`}
+                        alt=""
+                    />
+                    <div className="w-full hidden group-hover:flex justify-center">
+                        <a href={`https://discord.gg/rJWYVjc`}>
+                            <b className="text-serif">DISCORD</b>
+                        </a>
+                    </div>
+                </div>
+                {/* gmail */}
+                <div
+                    key="gmail"
+                    className={`mb-5 rounded-full border h-24 w-24 mx-auto flex items-center bg-[#DB4437] text-white group hover:w-full hover:justify-start transition duration-300 ease-in-out trans-item`}
+                >
+                    <img
+                        src={`https://i.ibb.co/sV69Yvx/gmail-logo-removebg-preview.png`}
+                        className={`ml-[15px] scale-x-[1.35] scale-y-[1.2] mt-2 w-16 h-16 object-cover transition 
+                            group-hover:animate-bounce`}
+                        alt=""
+                    />
+                    <div className="w-full hidden group-hover:flex justify-center">
+                        <b className="text-serif">sunthanhan1308@gmail.com</b>
+                    </div>
+                </div>
+                {/* phone */}
+                <div
+                    key="phone"
+                    className={`mb-5 rounded-full border h-24 w-24 mx-auto flex items-center bg-[#34c934] text-white group hover:w-full hover:justify-start transition duration-300 ease-in-out trans-item`}
+                >
+                    <img
+                        src={`https://i.ibb.co/CJgPyWw/phone-call-icon.png`}
+                        className={`ml-[15px] mt-2 w-16 h-16 object-cover transition 
+                            group-hover:animate-ringging`}
+                        alt=""
+                    />
+                    <div className="w-full hidden group-hover:flex justify-center">
+                        <b className="text-serif">+84 978699454</b>
+                    </div>
+                </div>
+                {/* facebook */}
+                <div
+                    key="facebook"
+                    className={`mb-5 rounded-full border h-24 w-24 mx-auto flex items-center bg-[#3B5998] text-white group hover:w-full hover:justify-start transition duration-300 ease-in-out trans-item cursor-pointer`}
+                >
+                    <img
+                        src={`https://i.ibb.co/TBb671x/facebook-logo.png`}
+                        className={`ml-[15px] mt-2 w-16 h-16 object-cover transition 
+                            group-hover:animate-pulse`}
+                        alt=""
+                    />
+                    <div className="w-full hidden group-hover:flex justify-center">
+                        <a href="https://www.facebook.com/meomattroi1308/" target="_blank">
+                            <b className="text-serif">FACEBOOK</b>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default index
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,40 @@
 module.exports = {
     content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
     theme: {
-        screens: {
-            'xxs': "480px",
-            'sm': "640px",
-            'md': "768px",
-            'lg': "1024px",
-            'xl': "1280px",
-            "2xl": "1536px",
+        extend: {
+            keyframes: {
+                shaking: {
+                    "0%": {
+                        transform: "rotate(20deg)",
+                    },
+                    "50%": {
+                        transform: "rotate(-20deg)",
+                    },
+                    "100%": {
+                        transform: "rotate(360deg)",
+                    },
+                },
+                ringging: {
+                    "0%": {
+                        transform: "rotate(-40deg)",
+                    },
+                    "50%": {
+                        transform: "rotate(20deg)",
+                    },
+                },
+            },
+            animation: {
+                shaking: "shaking 2s infinite alternate",
+                ringging: "ringging 1s infinite alternate",
+            },
+            screens: {
+                xxs: "480px",
+                sm: "640px",
+                md: "768px",
+                lg: "1024px",
+                xl: "1280px",
+                "2xl": "1536px",
+            },
         },
     },
     plugins: [
