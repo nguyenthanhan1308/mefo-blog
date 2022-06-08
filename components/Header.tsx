bff80fec5181d15a65e3fb5176e849d24a7d01a9
Thanh An~ <sunthanhan1308@gmail.com>
2022-06-08 09:56:28 +0700

    dark mode

--- components/Header.tsx
+++ components/Header.tsx
@@ -27,9 +27,11 @@ function Header() {
                 </div>
             </div>
             <div className="hidden md:flex items-center space-x-5 text-green-600 child:cursor-pointer">
-                <button className="" onClick={setCurrentTheme}>
-                    Theme
-                </button>
+                <label className="inline-flex relative items-center mr-5 cursor-pointer">
+                    <input type="checkbox" value="" id="green-toggle" className="sr-only peer" onChange={setCurrentTheme}/>
+                    <div className="w-11 h-6 bg-gray-200 rounded-full peer dark:bg-gray-700 peer-focus:ring-4 peer-focus:ring-green-300 dark:peer-focus:ring-green-800 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-green-600"></div>
+                    <span className="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300">Dark mode</span>
+                </label>
                 {/* <h3>Sign In</h3>
                 <h3 className="border px-4 py-1 rounded-full border-green-600">Get Started</h3> */}
             </div>
