f5fa8b482e5ae5430b127336eb455ef41a109c13
Thanh An~ <sunthanhan1308@gmail.com>
2022-06-10 08:53:34 +0700

    black loading

--- components/CatLoading.tsx
+++ components/CatLoading.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
 const CatLoading = () => {
-    return (     
-        <div className="loading-cat">
-            <div className="cat-body"></div>
-            <div className="cat-animation-mask"></div>
-            <div className="cat-head">
+    return (
+        <div className="loading-cat ">
+            <div className="cat-body dark:before:bg-[#121212]"></div>
+            <div className="cat-animation-mask dark:bg-[#121212] dark:before:bg-[#121212] dark:after:bg-[#121212]"></div>
+            <div className="cat-head dark:bg-[#121212]">
                 <div className="cat-face"></div>
                 <div className="cat-ear"></div>
                 <div className="cat-hand"></div>
@@ -15,7 +15,7 @@ const CatLoading = () => {
                 <div className="cat-beard left"></div>
                 <div className="cat-beard right"></div>
             </div>
-            <div className="cat-foot">
+            <div className="cat-foot dark:bg-[#121212]">
                 <div className="cat-belly"></div>
                 <div className="cat-leg"></div>
                 <div className="cat-tail"></div>
