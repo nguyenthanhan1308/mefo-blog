5f4eda8309296b2d9a6c512b0c77d272a476b8ef
Thanh An~ <sunthanhan1308@gmail.com>
2022-06-10 10:55:39 +0700

    loading cat

--- components/LoadingCat.js
+++ components/LoadingCat.js
@@ -2,10 +2,12 @@ import React from 'react'
 import styles from './loading.module.css'
 function LoadingCat() {
     return (
-        <div className={styles.cat}>
-            <div className={styles.catbody}></div>
-            <div className={styles.animationmask}></div>
-            <div className={styles.head}>
+        <div className={`${styles.cat}`}>
+            <div className={`${styles.catbody} dark:before:bg-[#121212]`}></div>
+            <div
+                className={`${styles.animationmask} dark:bg-[#121212] dark:before:bg-[#121212] dark:after:bg-[#121212]`}
+            ></div>
+            <div className={`${styles.head} dark:bg-[#121212]`}>
                 <div className={styles.face}></div>
                 <div className={styles.ear}></div>
                 <div className={styles.hand}></div>
@@ -15,7 +17,7 @@ function LoadingCat() {
                 <div className={`${styles.beard} ${styles.left}`}></div>
                 <div className={`${styles.beard} ${styles.right}`}></div>
             </div>
-            <div className={styles.foot}>
+            <div className={`${styles.foot} dark:bg-[#121212]`}>
                 <div className={styles.belly}></div>
                 <div className={styles.leg}></div>
                 <div className={styles.tail}></div>
