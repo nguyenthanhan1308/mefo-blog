0a5c366661ad2de3c490bb32ff1b404698e6cd60
THANHAN <sunthanhan1308@gmail.com>
2023-08-09 08:55:41 +0700

    remove young buffalo things

---
+++ pages/about/hobbies.tsx
@@ -0,0 +1,67 @@
+{/* second image */}
+<div className="second-trans">
+<div className="width-trans group transition duration-200 flex justify-center md:h-[204px] lg:h-[276px] xl:h-[360px]">
+    <div className="relative trans-item pr-4 text-sm md:text-2xl text-right w-[364px] group-hover:w-[240px] lg:w-[492px] lg:group-hover:w-[328px] xl:w-[640px] xl:group-hover:w-[415px] ">
+        <p className="group-hover:opacity-0 transition duration-300 ease-in-out">A games enjoyer.</p>
+        <p className="group-hover:opacity-100 opacity-0 top-0 right-2 absolute transition duration-2000 ease-in-out">
+            lol <br />
+            Csgo <br />
+            dota <br />
+            Pokemon <br />
+            Valorant <br />
+            Final Fantasy
+        </p>
+    </div>
+    <div className="box-border trans-item w-[364px] group-hover:w-[485px] lg:w-[492px] lg:group-hover:w-[656px] xl:w-[640px] xl:group-hover:w-[855px]">
+        <img
+            className="dark:shadow-white aspect-video shadow-lg shadow-black group-hover:hidden transition duration-300"
+            src="https://i.ibb.co/ky65KBf/val-cs.jpg"
+            alt="csval"
+        />
+        <img
+            className="dark:shadow-white aspect-video shadow-lg shadow-black group-hover:inline hidden transition duration-300"
+            src="https://i.ibb.co/4NNR6VZ/starter.jpg"
+            alt="starter"
+        />
+    </div>
+</div>
+<div className="second-trans mx-auto w-full mb-40">
+    <hr className="max-w-lg w-full my-5 mx-auto border dark:border-yellow-500 border-green-500" />
+    <div className="flex flex-col mb-2 md:mb-5">
+        <p className="underline decoration-green-500 underline-offset-4 mx-auto text-lg md:text-3xl">
+            MY HOBBIES{" "}
+        </p>
+    </div>
+    <div className="flex justify-between">
+        {/* hobby */}
+        <div className="hobby text-xs md:text-lg gradient-animated shadow w-[8rem] md:w-[245px] lg:w-[330px] xl:w-[410px] group ">
+            Gaming
+            <div className="opacity-0 group-hover:opacity-100 transition duration-100 ease">
+                Valorant
+                <br />
+                CSGO
+                <br />
+            </div>
+        </div>
+        <div className="hobby text-xs md:text-lg gradient-animated shadow w-[8rem] md:w-[245px] lg:w-[330px] xl:w-[410px] group">
+            Programming
+            <div className="opacity-0 group-hover:opacity-100 transition duration-100 ease">
+                Javascript
+                <br />
+                ReactJS
+                <br />
+                TypeJS
+                <br />
+                NextJS
+                <br />
+                NodeJS
+                <br />
+            </div>
+        </div>
+        <div className="hobby text-xs md:text-lg gradient-animated shadow w-[8rem] md:w-[245px] lg:w-[330px] xl:w-[410px] group">
+            Reading
+            <div className="opacity-0 group-hover:opacity-100 transition duration-100 ease"></div>
+        </div>
+    </div>
+</div>
+</div>
